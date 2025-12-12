bb107647464cb85284090b9da31cf146288f39cb
Bartosz Godzieszka <bartosz.godzieszka@o2.pl>
2025-12-12 20:04:33 +0100

    menu change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import sys
+from turtle import title
 import pygame
 from gameMainMechanics import GameMechanic
 from textDisplay import text
@@ -10,11 +11,11 @@ pygame.init()
 backgroundColor = (48 , 25,52)
 txtColor = (255,255,255)
 boxColor = (54,15,90)
-
+titleFontColor =  (255,255,102)
 
 
 #Wielkość okna gry
-screen = pygame.display.set_mode((600,700))
+screen = pygame.display.set_mode((600,720))
 
 dif = Difficulty()
 text = text()
@@ -28,13 +29,15 @@ blockFallTimer = dif.blockFallTimer
 
 
 title_txt = text.renderTitle("TETRIS", txtColor)
-menuTitle_txt = text.renderBigText("TETRIS" , txtColor)
+menuTitle_txt = text.renderBigText("TETRIS" , titleFontColor)
 score_txt = text.renderText("SCORE:", txtColor)
 next_txt = text.renderText("NEXT:", txtColor)
 gameOver_txt = text.renderText("GAME OVER", txtColor)
 maxScore_txt= text.renderSmallText("MAX SCORE:", txtColor)
 SoundtrackCredit = text.renderSmallText("Soundtrack: Kim Lightyear", txtColor)
 AudioEffectsCredit = text.renderSmallText("Sound Effects: floraphonic", txtColor)
+FontCredit = text.renderSmallText("Font: tylerdunn99", txtColor)
+Author_txt = text.renderSmallText("Bartosz Godzieszka", txtColor)
 start_txt = text.renderText("PRESS SPACE TO START" , txtColor)
 selectDif_txt = text.renderSmallText("Use arrows <- -> to change difficulty",txtColor)
 easy_txt = text.renderText("< EASY >" , txtColor)
@@ -83,10 +86,12 @@ while menuRunning:
     screen.fill(backgroundColor)
     pygame.draw.rect(screen, boxColor, difficultyBackground,0,10)
     screen.blit(start_txt , (20,250,50,50))
-    screen.blit(menuTitle_txt  , (110,40,50,50))
+    screen.blit(menuTitle_txt  , (110,80,50,50))
     screen.blit(selectDif_txt, (20,310,50,50))
     screen.blit(SoundtrackCredit,(20,670,50,50))
     screen.blit(AudioEffectsCredit, (20,645,50,50))
+    screen.blit(FontCredit, (20,695,50,50))
+    screen.blit(Author_txt, (10,10,50,50))
     
     #Wyświetlanie aktualnego poziomu trudności
     if dif.blockFallTimer == dif.easy:
@@ -141,6 +146,7 @@ while isRunning:
     screen.blit(score_txt , (20,80,50,50))
     screen.blit(SoundtrackCredit,(20,670,50,50))
     screen.blit(AudioEffectsCredit, (20,645,50,50))
+    screen.blit(FontCredit, (20,695,50,50))
     screen.blit(maxScore_txt,(20,570,50,50))
     pygame.draw.rect(screen, boxColor, scoreBackground, 0 ,10)
     
